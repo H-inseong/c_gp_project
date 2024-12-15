2164c549ead9fd58748e98993c873ecb79d52a3e
hwanginseong@laptop <3dlstjd3@gmail.com>
2024-12-15 22:16:15 +0900

    이동 범위 제한

--- c_gp_project/Camera.cpp
+++ c_gp_project/Camera.cpp
@@ -49,6 +49,11 @@ void Camera::processKeyboardInput(int key, float deltaTime) {
     if (key == 'a') mPosition -= rightXZ * velocity;
     if (key == 'D') mPosition += rightXZ * velocity;
     if (key == 'd') mPosition += rightXZ * velocity;
+
+    if (mPosition.x > 29.f) mPosition.x = 29.f;
+    if (mPosition.x < -29.f) mPosition.x = -29.f;
+    if (mPosition.z > 30.f) mPosition.z = 30.f;
+    if (mPosition.z < -14.f) mPosition.z = -14.f;
     
 }
 
